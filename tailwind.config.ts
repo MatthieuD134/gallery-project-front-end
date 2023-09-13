acc2880655d00d3f7d3b6f793ea81a0467796c17
Matthieu Daulhiac <96766017+MatthieuD134@users.noreply.github.com>
2023-09-13 15:20:02 +0800

    manual fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,14 @@
-import type { Config } from 'tailwindcss';
-import { fontFamily } from 'tailwindcss/defaultTheme';
-
-import { shadcnPreset } from './src/lib/shadcn-preset';
+import type { Config } from "tailwindcss";
+import { shadcnPreset } from "./src/lib/shadcn-preset";
 
 const config = {
   presets: [shadcnPreset],
-  darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
 } satisfies Config;
 
 export default config;
