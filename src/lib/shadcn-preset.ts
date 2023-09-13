1a05169ca119d1285096e24c4583fbe564c321a4
Matthieu Daulhiac <96766017+MatthieuD134@users.noreply.github.com>
2023-09-13 14:56:00 +0800

    add tailwind configuration and shadcn config

---
+++ src/lib/shadcn-preset.ts
@@ -0,0 +1,9 @@
+import type {Config} from "tailwindcss";
+import { shadcnPlugin } from "./shadcn-plugin";
+import animatePlugin from "tailwindcss-animate";
+
+export const shadcnPreset = {
+    darkMode: ["class"],
+    content: [],
+    plugins: [animatePlugin, shadcnPlugin]
+} satisfies Config;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,17 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
-const config: Config = {
+import { shadcnPreset } from './src/lib/shadcn-preset';
+
+const config = {
+  presets: [shadcnPreset],
+  darkMode: ["class"],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
+} satisfies Config;
+
+export default config;
